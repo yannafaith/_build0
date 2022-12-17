660513502e34a421db0f959224223ee81eb47d00
yanna <yannafaith97@gmail.com>
2022-12-17 04:13:46 -0500

    adds wave struct and mapping

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,25 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaveCnt;
+    struct Wave  {
+        address sender;
+        string message;
+    }
+
+    Wave wave = Wave(msg.sender,"Hello World");
+
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function newWave() public {
+        totalWaveCnt += 1;
+        console.log("%d has waved and sent message %s!", wave.sender, wave.message);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaveCnt);
+        return totalWaveCnt;
     }
 }
