cbb1166d2b1d19c322ccab78e6360d01a7ad03a3
yanna <yannafaith97@gmail.com>
2022-12-18 17:40:56 -0500

    impl waves storage

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,20 +6,30 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaveCnt;
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
     struct Wave  {
         address sender;
         string message;
+        uint256 timestamp;
     }
 
-    Wave wave = Wave(msg.sender,"Hello World");
+    Wave[] waves;
 
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
     }
 
-    function newWave() public {
+    function newWave(string memory _message) public {
         totalWaveCnt += 1;
-        console.log("%d has waved and sent message %s!", wave.sender, wave.message);
+        console.log("%d has waved and sent message %s!", msg.sender, _message);
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
